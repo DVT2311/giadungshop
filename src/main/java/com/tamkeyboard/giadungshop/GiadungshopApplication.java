f1d9c4042ccb4effeeca243519eaf1e0d3f7ee55
Tam KeyBoard <dinhvantam333@gmail.com>
2025-08-05 17:24:16 +0700

    init project

---
+++ src/main/java/com/tamkeyboard/giadungshop/GiadungshopApplication.java
@@ -0,0 +1,13 @@
+package com.tamkeyboard.giadungshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiadungshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiadungshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamkeyboard/giadungshop/GiadungshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamkeyboard.giadungshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiadungshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

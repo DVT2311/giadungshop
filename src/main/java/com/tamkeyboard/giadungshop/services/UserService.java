a2a0b14bfa112275a766a76864b33c6f7bbdeaeb
Tam KeyBoard <dinhvantam333@gmail.com>
2025-08-06 15:23:20 +0700

    done 54

---
+++ src/main/java/com/tamkeyboard/giadungshop/services/UserService.java
@@ -0,0 +1,10 @@
+package com.tamkeyboard.giadungshop.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String handleHello() {
+        return " Hello from service";
+    }
+}

9b0f8061f72fbde14e2c8c6194c4bc5aabe3f772
Tam KeyBoard <dinhvantam333@gmail.com>
2025-08-07 09:00:48 +0700

    done 79

--- src/main/java/com/tamkeyboard/giadungshop/domain/User.java
+++ src/main/java/com/tamkeyboard/giadungshop/domain/User.java
@@ -1,9 +1,14 @@
 package com.tamkeyboard.giadungshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,6 +24,14 @@ public class User {
     private String address;
     private String phone;
     private String avatar;
+
+    // User many -> to one -> role
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    List<Order> orders;
 
     public long getId() {
         return id;
